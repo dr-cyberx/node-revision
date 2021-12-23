2f2d040a14c16f6bee1c4c0b7ce08a3ec1bf18b0
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-12-23 13:40:08 +0530

    changes

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
-import express, { Response, Request } from 'express';
+import express from 'express';
 import path from 'path';
 import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
+import errorController from './controllers/error';
 
 const app: express.Application = express();
 
@@ -14,9 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req: Request, res: Response) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController);
 
 app.listen(4000, () => {
   console.log(`the server is up at http://localhost:${4000}`);
