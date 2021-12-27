e05495c84ae41a54840ef6af132ba057ab8499ab
ItsMeVishal007 <vishal.codedrill@gmail.com>
2021-12-27 18:41:33 +0530

    changes

--- routes/admin.ts
+++ routes/admin.ts
@@ -2,11 +2,14 @@ import express, { Router } from 'express';
 import {
   getAddProduct,
   postAddProduct,
+  getProducts,
 } from '../controllers/admin';
 
 const router: Router = express.Router();
 
 router.get('/add-product', getAddProduct);
+
+router.get('/products', getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', postAddProduct);
